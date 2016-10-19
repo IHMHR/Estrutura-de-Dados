f72a1bcad61ba3bd940c4b9b16709e8510d705ea
IHMHR <martinelli.igor@hotmail.com>
2016-10-18 22:13:17 -0200

    Lista Circular

--- ED-2/ListaCircular.c
+++ ED-2/ListaCircular.c
@@ -19,10 +19,21 @@ struct lista
 	Tlista *prox;
 };
 
+int isListavazia(Tlista *lst)
+{
+	if(NULL == lst)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 Tlista* inserir(Tlista *anterior, char letra)
 {
 	Tlista *aux = (Tlista*) malloc(sizeof(Tlista));
-	Tlista *aux2;
 	if(NULL == anterior)
 	{
 		aux->dado = letra;
@@ -31,7 +42,22 @@ Tlista* inserir(Tlista *anterior, char letra)
 	}
 	else
 	{
-		//dado
+
+		Tlista *auxN = anterior;
+		aux->dado = letra;
+		aux->prox = aux;
+		aux->prox = anterior;
+
+		do {
+			auxN = auxN->prox;
+		} while (auxN->prox != anterior);
+
+		auxN->prox = aux;
+
+		return aux;
+
+
+		/*//dado
 		//ligação
 		do
 		{ // para no ultimo da lista
@@ -39,9 +65,10 @@ Tlista* inserir(Tlista *anterior, char letra)
 			/*aux->dado = letra;
 			aux->prox = aux2;
 			aux2->prox = aux;*/
-		} while(aux2->prox != anterior);
+		/*} while(aux2->prox != anterior);
 		//ultimo no primeiro
-		return aux;
+		aux2->prox = anterior;
+		return anterior;*/
 	}
 }
 
@@ -53,13 +80,79 @@ void imprime(Tlista *lst)
 	}
 	else
 	{
-		Tlista *aux = lst->prox;
-		while(aux != lst)
+		Tlista *aux = lst;
+		do
 		{
 			printf("%c\n", lst->dado);
 			lst = lst->prox;
-		}
+		} while (aux != lst);
 	}
+}
+
+Tlista* BuscaCircular(Tlista* lst, char letra)
+{
+	if(isListavazia(lst))
+	{
+		return NULL; //return lst;
+	}
+	else
+	{
+		Tlista *aux = lst;
+		do
+		{
+			if(lst->dado == letra)
+			{
+				return lst;
+			}
+			lst = lst->prox;
+		} while (aux != lst);
+		return NULL;
+	}
+}
+
+Tlista* liberaCircular(Tlista *lst)
+{
+	/*while(!isListavazia(lst))
+	{
+		Tlista *aux = lst;
+		Tlista *aux2 = lst;
+		lst = lst->prox;
+
+		do
+		{
+			aux2 = aux2->prox;
+		} while (aux != aux2->prox);
+
+		free(aux);
+		aux2->prox = lst;
+	}
+
+	return lst;*/
+
+	Tlista *aux = lst;
+	Tlista *aux2 = lst->prox;
+
+	if (aux->prox == aux)
+	{
+		free(aux);
+		return NULL;
+	}
+	else
+	{
+		do
+		{
+			aux = aux2->prox;
+			free(aux2);
+			lst->prox = aux;
+
+			// reposicionar aux2
+			aux2 = aux;
+		} while (aux != lst);
+
+		free(aux);
+	}
+
+	return NULL;
 }
 
 int main(void)
@@ -71,6 +164,15 @@ int main(void)
 	listaCompleta = inserir(listaCompleta, 'E');
 
 	imprime(listaCompleta);
+
+	printf("\n");
+
+	printf("%p", BuscaCircular(listaCompleta, 'A'));
+
+	printf("\n\n");
+
+	listaCompleta = liberaCircular(listaCompleta);
+	printf("%s", isListavazia(listaCompleta) ? "Lista Vazia" : "Lista CHEIA");
 
 	return 0;
 }
